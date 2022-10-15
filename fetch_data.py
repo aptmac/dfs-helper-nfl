2f982ab3ea01396ffffe59ce81f8059e6e54f54c
aptmac <aptmac@users.noreply.github.com>
2022-10-14 21:12:58 -0400

    chore(filename): simplify rawdata file name for consistent chaining

--- fetch_data.py
+++ fetch_data.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 from bs4 import BeautifulSoup
-from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -22,7 +21,7 @@ def fetch_yahoo_data():
 def write_data(data, format):
     if not os.path.exists('./rawdata'):
         os.makedirs('./rawdata')
-    data.to_csv('./rawdata/' + format + '-' + datetime.now().strftime("%d-%m-%Y-%Hh%Mm%Ss") + '.csv', encoding='utf-8', index=True)
+    data.to_csv('./rawdata/' + format + '.csv', encoding='utf-8', index=True)
 
 # Fetch all positional data from FP, return a DataFrame containing all the data for the week
 def fetch_fp_data():
@@ -111,9 +110,8 @@ def setup_yahoo_df():
 
     df = pd.DataFrame(text['players']['result'])
     df = df[df.gameStartTime.str.startswith('Sun')]
-    df.drop(['sport', 'playerCode', 'gameCode', 'homeTeam', 'awayTeam', 'gameStartTime'], axis=1, inplace=True)
+    df.drop(['sport', 'playerCode', 'gameCode', 'homeTeam', 'awayTeam', 'gameStartTime', 'fppg'], axis=1, inplace=True)
     df['name'] = df['name'].str.strip()
-    df.rename(columns = {'fppg':'yahoo'}, inplace=True)
     df.sort_values(by=['salary'], ascending=False, inplace=True)
     return df
 
